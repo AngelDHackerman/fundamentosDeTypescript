27a70e1f7c6a88a0fbf675ce2227bf187ac4c94d
angelDariaux <angeldariaux@gmail.com>
2022-05-11 19:49:31 -0600

    viendo los tipos de datos en TS

---
+++ src/02Demo2.ts
@@ -0,0 +1,1 @@
+const numbers: Number [] = [1, 2, 3, 4];
---
+++ src/03Typing.ts
@@ -0,0 +1,14 @@
+//todo: (() => {}) ();    Esta es una funcion anonima donde podemos meter todo nuestro codigo para que Ts no choque entre si.
+
+(() => {
+  let myProductName: string = 'Product 1';
+  let myProductPrice: number = 123;
+
+  myProductName = 'Changed';
+  myProductName.toLowerCase();
+  console.log(myProductName);
+
+  myProductPrice.toFixed();
+
+
+})();
